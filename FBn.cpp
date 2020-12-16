b4e7e6f767180872870379b63b50db951a7837fd
Andrey2102 <46657129+Andrey2102@users.noreply.github.com>
2020-12-16 07:14:23 +0200

    Add priority

--- FBn.cpp
+++ FBn.cpp
@@ -7,6 +7,14 @@
 #include <ctime>
 using namespace std;
 
+#include <iostream>
+#include <map>
+#include <vector>
+#include <chrono>
+#include <string>
+#include <thread>
+#include <ctime>
+using namespace std;
 
 
 class Task
@@ -17,14 +25,18 @@ class Task
 	void setDone() { done = (done) ? false : true; }
 public:
 	string getName() { return name; }
+	int getTime() { return time; }
 	bool isDone() { return done; }
 	void make(int time_to_make);
 	void make();
+
 	Task(int t, string n) {
 		time = t;
 		done = false;
 		name = n;
 	}
+	Task(){}
+
 };
 
 
@@ -39,24 +51,24 @@ void Task::make(int time_to_make) {
 		}
 		else {
 			cout << name << "		" << time << "..." << endl;
-			sleep(1);
+			this_thread::sleep_for(chrono::milliseconds(1000));
 			time--;
 		}
 	}
 }
 
 void Task::make() {
-	while(true) {
+	while (true) {
 		if (time == 0)
 		{
 			cout << name << " Done" << endl;
-			
+
 			this->setDone();
 			break;
 		}
 		else {
 			cout << name << "		" << time << "..." << endl;
-			sleep(1);
+			this_thread::sleep_for(chrono::milliseconds(1000));
 			time--;
 		}
 	}
@@ -69,35 +81,58 @@ class TaskSheduler
 	int size;
 
 	vector<int> quants;
-	
-	vector<queue<Task>> queues;
+
+	vector<map<int, Task>> queues;
+
+	bool stop;
 public:
-	int getQueuesCount(){ return queues.size(); }
-	
+
+	void setStop() { stop = (stop) ? false : true; }
+	int getQueuesCount() { return queues.size(); }
+
 	void work();
 
 	void process(int pos);
-	
-	int getQuant(int pos){ 
-		if (pos >= quants.size() || pos < 0){
+
+	int getQuant(int pos) {
+		if (pos >= quants.size() || pos < 0) {
 			cout << "Wrong position" << endl;
 			return -1;
 		}
-		else{
+		else {
 			return quants[pos];
 		}
 	}
-	
-	void addTask(Task t){
-		queues[0].push(t);
+
+	int prior(int time, int quant) {
+		if (time < quant / 2) {
+			return 0;
+		}
+		if (time < quant && time >= quant / 2) {
+			return 1;
+		}
+		if (time / 2 < quant) {
+			return 0;
+		}
+		if (time >= quant && time < quant * 2) {
+			return 2;
+		}
+		if (time >= quant * 2) {
+			return 3;
+		}
+	}
+
+	void addTask(Task t) {
+		queues[0].insert(pair<int, Task>(0, t));
 	}
 	TaskSheduler(int queues_count);
 
 };
 
 
-TaskSheduler::TaskSheduler(int queues_count){
-	queue <Task> t;
+TaskSheduler::TaskSheduler(int queues_count) {
+	map <int, Task> t;
+	stop = false;
 	if (queues_count < 3 || queues_count > 32)
 	{
 		quants.push_back(1);
@@ -106,17 +141,17 @@ TaskSheduler::TaskSheduler(int queues_count){
 		queues.push_back(t);
 		size = 2;
 	}
-	else{
+	else {
 		size = queues_count;
 		for (int i = 1; i <= queues_count; ++i)
 		{
-			
+
 			queues.push_back(t);
 			if (i == queues_count)
 			{
 				quants.push_back(-1);
 			}
-			else{
+			else {
 				quants.push_back(2 * i - 1);
 			}
 		}
@@ -124,65 +159,97 @@ TaskSheduler::TaskSheduler(int queues_count){
 }
 
 
-void TaskSheduler::process(int pos){
-	
+void TaskSheduler::process(int pos) {
+
 	cout << "		QUEUE " << pos + 1 << endl;
 
 	int tmp = queues[pos].size();
-	
+
 	if (quants[pos] == -1)
 	{
 		for (int i = 0; i < tmp; ++i)
 		{
-			queues[pos].front().make();
-			queues[pos].pop();
+			(queues[pos][0]).make();
+			queues[pos].erase(queues[pos].begin());
 		}
 	}
-	else{
+	else {
 		for (int i = 0; i < tmp; ++i)
 		{
-			queues[pos].front().make(quants[pos]);
-			if (!queues[pos].front().isDone())
+			queues[pos][0].make(quants[pos]);
+			if (!queues[pos][0].isDone())
 			{
-				queues[pos + 1].push(queues[pos].front());
+				queues[pos + 1].insert(pair<int, Task> (prior(queues[pos][0].getTime(), quants[pos]), queues[pos][0]));
 			}
-			queues[pos].pop();
+			queues[pos].erase(queues[pos].begin());
 		}
 	}
 }
 
 
-void TaskSheduler::work(){
-	bool empty = false;
-	while(!empty){
-		empty = true;
-		for (int i = 0; i < size; ++i)
+void TaskSheduler::work() {
+	bool empty;
+	while (!stop) {
+		empty = false;
+		while (!empty) {
+			empty = true;
+			for (int i = 0; i < size; ++i)
+			{
+				if (!queues[i].empty())
+				{
+					empty = false;
+					process(i);
+					break;
+				}
+			}
+		}
+	}
+}
+
+
+string nameF() {
+	string name;
+	string tmp1 = "0123456789";
+	string tmp2 = "qwertyuioplkjhgfdsazxcvbnm";
+	name = tmp2[rand() % 25];
+	int var = rand() % 15;
+	for (int i = 1; i < 8; ++i)
+	{
+		if (var % 2 == 0)
 		{
-			if (!queues[i].empty())
-			{
-				empty = false;
-				process(i);
-				i = 0;
-			}
+			name += tmp1[rand() % 10];
+			var = rand() % 15;
+		}
+		else {
+			name += tmp2[rand() % 25];
+			var = rand() % 15;
 		}
 	}
+	return name;
 }
 
-
-
-
-void th_inp(int time_limit, int count, TaskSheduler &ts){
+void th_inp(int time_limit, int count, TaskSheduler &ts) {
 	srand(time(NULL));
 	int tmp = 0;
 	for (int i = 0; i < count; ++i)
 	{
 		tmp = rand() % time_limit + 1;
-		Task t(tmp, "new Task");
+		Task t(tmp, nameF());
 		ts.addTask(t);
-		sleep(5);
+		this_thread::sleep_for(chrono::milliseconds(rand()%5000));
 	}
+	ts.setStop();
 }
 
+int main() {
+	TaskSheduler ts(10);
+	thread inp_thread(th_inp, 13, 6, ref(ts));
+	
+	ts.work();
+	inp_thread.join();
+
+	return 0;
+}
 int main() {
 	TaskSheduler ts(3);
 	thread inp_thread(&th_inp, 7, 4, ts);
